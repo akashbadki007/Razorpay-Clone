f52e6d02a3b5d9e106f2676ed6b7e0c130657f23
Akash Anil Badki <55804895+akashbadki007@users.noreply.github.com>
2024-07-28 14:06:02 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+      mullish: ["Mulish", "sans-serif"],
+    },
+    colors: {
+      Blue1:"#eef9fe",
+      VeryPaleWhiteBlue:"#edf7ff",
+      LightGrayishBlue:"#f5f8fe",
+      SlateGray:"#5a6682",
+      CatalinaBlue:"#2b3c5e",
+      Vividblue:"#191ae9",
+      deepBlue: "#02042a ",
+      lightBlue: "#2b84ea",
+      lightBlue300: "#4b94ed",
+      lightBlue500: "#0b72e7",
+      greenLight: "#61cea6",
+      grayText: "#818597",
+      lightGray: "#e2e2e2",
+      grayBlue: "#344a6c",
+      deepBlueHead: "#162f56",
+      gray2: "#525a76",
+    },},
+  },
+  plugins: [],
+}
